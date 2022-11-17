ff0b1cf3ed7fdf880cace9f17142502edfd2b9a5
saurav4986 <saurav.raj4986@gmail.com>
2022-11-17 13:15:32 +0530

    Added: Welcome message

---
+++ src/com/bridgeLabz/LinkedList/SimpleLinkedList.java
@@ -0,0 +1,8 @@
+package com.bridgeLabz.LinkedList;
+
+public class SimpleLinkedList {
+    public static void main(String[] args) {
+
+        System.out.println("!!!Welcome to Linked List!!!");
+    }
+}
